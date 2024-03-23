1d22c83e2b9b75012265c08eda9f8d8d83cc2cb3
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2024-03-23 11:00:54 -0300

    Updating gevent examples

---
+++ gevent/gevent Oficial Tutorial/concurrent_download.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import gevent
+from gevent import monkey
+monkey.patch_all()
+import requests
+
+urls = [
+    'https://www.google.com/',
+    'https://www.apple.com/',
+    'https://www.python.org/'
+]
+
+
+def print_head(url):
+    print('Starting %s' % url)
+    data = requests.get(url).text
+    print('%s: %s bytes: %r' % (url, len(data), data[:50]))
+
+
+jobs = [gevent.spawn(print_head, _url) for _url in urls]
+gevent.wait(jobs)
