0343259c10b634c7ddc46b22e823e8ecd87220c6
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2024-04-03 16:34:46 -0300

    Including middlewares

--- aiohttp/Introduction/aiohttpdemo_polls/main.py
+++ aiohttp/Introduction/aiohttpdemo_polls/main.py
@@ -2,13 +2,17 @@ from aiohttp import web
 import aiohttp_jinja2
 import jinja2
 
-from routes import setup_routes
+from routes import setup_routes, setup_static_routes
+from middlewares import setup_middlewares
 from settings import config, BASE_DIR
 from db import mysql_context
+
 
 app = web.Application()
 app['config'] = config
 aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(str(BASE_DIR / 'aiohttpdemo_polls' / 'templates')))
+setup_static_routes(app)
 setup_routes(app)
+setup_middlewares(app)
 app.cleanup_ctx.append(mysql_context)
 web.run_app(app)
--- aiohttp/Introduction/aiohttpdemo_polls/routes.py
+++ aiohttp/Introduction/aiohttpdemo_polls/routes.py
@@ -1,4 +1,11 @@
 from views import index, poll
+from settings import BASE_DIR
+
+
+def setup_static_routes(app):
+    app.router.add_static('/static/',
+                          path=BASE_DIR / 'aiohttpdemo_polls' / 'static',
+                          name='static')
 
 
 def setup_routes(app):
